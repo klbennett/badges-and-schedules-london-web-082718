bed1231aaddef2b6e72547ccf8cdfe5c31c42c73
Kay B <kaylouisebennett@gmail.com>
2018-08-14 15:19:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,6 @@ array.each do |name|
   puts "Hello, my name is #{name}."
 end
 
-end
-
 def assign_rooms(array)
   
   their_room << array.each_with_index {|name, room| puts "Hello, #{name}! You'll be assigned to room #{room}!"}
