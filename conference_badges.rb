eda2be7c2f8efbfc296dc98366c2180e2ef62db5
Kay B <kaylouisebennett@gmail.com>
2018-08-14 14:55:59 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-array.each {|item| puts "Hello, my name is #{item}"}
+array.each {|item| puts "Hello, my name is #{item}."}
 end
 
 def assign_rooms(array)
