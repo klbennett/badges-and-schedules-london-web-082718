9787becccb9037def154a01195ed23c1e5cc3296
Kay B <kaylouisebennett@gmail.com>
2018-08-14 14:22:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+array_of_names.each {|x| print "Hello, my name is #{x}." }
+end
