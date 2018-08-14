edce4b48a944936c2fffbfc35d7723beae4310e9
Kay B <kaylouisebennett@gmail.com>
2018-08-14 14:42:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,6 @@ def batch_badge_creator(array)
 array.each {|item| puts "Hello, my name is #{item}"}
 end
 
-def assign rooms(array)
-  each 
-  array.each_with_index {|item| puts "Hello, #{name}! You'll be assigned to room #{item}!"}
+def assign_rooms(array)
+  
+  array.each_with_index {|name, room| puts "Hello, #{name}! You'll be assigned to room #{room}!"}
